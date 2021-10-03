a0566c8dfddb41d662fdc1cdf374fae42ea76809
Caleb Pentecost <caleb.james.pentecost@gmail.com>
2021-10-03 00:28:40 -0700

    Took devin's scene and added camp, attacks

---
+++ project/levels/Hero.gd
@@ -0,0 +1,72 @@
+extends MeshInstance
+
+var attack_charged = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _get_melee_enemies():
+	return $AttackZone.get_overlapping_bodies()
+
+func _attack_single_enemy():
+	var enemies = _get_melee_enemies()
+	var closest_distance = INF
+	var closest_enemy = null
+	for enemy in enemies:
+		var distance = self.global_transform.origin.distance_squared_to(enemy.global_transform.origin)
+		if distance < closest_distance:
+			closest_distance = distance
+			closest_enemy = enemy
+	if closest_enemy == null:
+		# Nothing to do...
+		return
+	
+	# Attack the enemy
+	print("Attacking enemy: " + closest_enemy.to_string())
+	var slashscn = load("res://objects/effects/slash.tscn")
+	var slash = slashscn.instance()
+	slash.global_transform.origin = self.global_transform.origin
+	slash.look_at(closest_enemy.global_transform.origin, Vector3(0, 1, 0))
+	slash.rotate_object_local(Vector3(0,1,0), 3.14)
+	self.get_parent().add_child(slash)
+
+func _attack_all_enemies():
+	# Hit 'em all
+	var enemies = _get_melee_enemies()
+	for enemy in enemies:
+		print("Attacking enemy: " + enemy.to_string())
+	var spinscn = load("res://objects/effects/spin.tscn")
+	var spin = spinscn.instance()
+	spin.global_transform.origin = self.global_transform.origin
+	self.get_parent().add_child(spin)
+
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == 1:
+			if event.pressed == true:
+				# Sword pressed
+				_attack_single_enemy()
+				attack_charged = false
+				$AttackChargeTimer.start()
+			else:
+				$AttackChargeTimer.stop()
+				# Charged?
+				if attack_charged == true:
+					# spin attack
+					_attack_all_enemies()
+				else:
+					# Not long enough
+					pass
+		if event.button_index == 2:
+			if event.pressed == true:
+				# Do nothing on press
+				pass
+			else:
+				# Arrow attack
+				pass
+
+
+func _on_AttackChargeTimer_timeout():
+	attack_charged = true
+	$AttackChargeTimer.stop()
---
+++ project/objects/effects/slash.gd
@@ -0,0 +1,21 @@
+extends Spatial
+
+export(float) var lifetime = 0.20
+export(float) var speed = 4.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$Lifetime.wait_time = lifetime
+	$Lifetime.start()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	translate(Vector3(0, 0, delta * speed))
+	pass
+
+
+func _on_Lifetime_timeout():
+	for child in self.get_children():
+		child.queue_free()
+	self.queue_free()
